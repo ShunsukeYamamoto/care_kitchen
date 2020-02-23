f8eb04b4cbc09ec62b41a87c5e235f71651924bf
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-02-23 17:50:29 +0900

    ユーザー、パーソンモデル作成
    外部キー、各カラム作成

---
+++ db/migrate/20200223081656_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name, null: false
+      t.float :height, null: false
+      t.float :weight, null:false
+      t.string :gender, null: false
+
+      t.timestamps
+    end
+  end
+end
