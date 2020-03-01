9e6b4d863159b7e89edd97e6f780dc486ba9b91f
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-03-01 14:42:32 +0900

    ユーザーログイン機能、新規登録機能エラーハンドリング

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,12 +14,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new(sign_up_params)
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
-      render :new
+      render :new and return
     end
     session["create_acount"] = { user: @user.attributes}
     session["create_acount"][:user]["password"] = params[:user][:password] 
     @person = @user.people.build
-    # binding.pry
     render :new_first_person
   end
 
@@ -31,11 +30,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render :new_first_person and return
     end
     @user.people.build(@person.attributes)
-    # binding.pry
     @user.save
     @person[:user_id] = @user.id
     sign_in(:user, @user)
-    binding.pry
     redirect_to root_path
   end
 
