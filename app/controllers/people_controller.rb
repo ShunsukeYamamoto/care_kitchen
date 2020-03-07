4f0447872b02b5c44e2c830e25e21e4dfd754ea6
shuntaro <shun.sea.f@gmail.com>
2020-03-08 02:07:53 +0900

    ビュー編集

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,16 +4,16 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
     informations = @person.personal_informations.last(14)
     @bmis=[]
+    @days=[]
     informations.each do |information|
       @bmis << information.bmi
+      @days << information.date.strftime("%m/%d") 
     end
-    binding.pry
     new_information = informations.last
     new_weight = new_information.weight
     @new_height = new_information.height
     @new_bmi = (new_weight/@new_height/@new_height).round(1)
     @personalinformation=@person.personal_informations.new
-    
   end
 
 
